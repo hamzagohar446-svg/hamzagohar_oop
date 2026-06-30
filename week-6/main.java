a8e2d5e2589b747e6a8d6ca162c9f6199f71a10b
hamzagohar446-svg <hamzagohar446@gmail.com>
2026-06-30 02:11:15 -0700

    Add main method to instantiate Lecturer
    Implemented main method to create and display Lecturer information.

---
+++ week-6/main.java
@@ -0,0 +1,9 @@
+package week-6;
+ublic static void main(String[] args) {
+  // Create an instance of Lecturer with the required details
+  Lecturer Lecturer = new Lecturer ("L100", "Dr Ahmed", " Java Programming", "Faculty of Information Technology"); 
+// Display the Lecturer's informattion 
+Lecturer.displayInfo();
+  Lecturer.displaysubjects();
+}
+} 
